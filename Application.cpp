f2d7b500849b4a27600f5824b0f12f6e39889674
sv <commanderkeen83@googlemail.com>
2017-04-22 13:53:40 +0200

    Application.hpp 	Button.cpp 	Button.hpp 	CMakeLists.txt 	Calculator.cpp 	Calculator.hpp 	Compute.cpp 	Compute.hpp 	Display.cpp 	Display.hpp 	main.cpp

---
+++ Application.cpp
@@ -0,0 +1,35 @@
+//
+// Created by keen on 19/04/2017.
+//
+
+#include "Application.hpp"
+#include <SFML/Window/Event.hpp>
+Application::Application()
+: mWindow(sf::VideoMode{260,350}, "Ellens Calculator"), mCalculator(&mWindow){
+
+}
+
+
+void Application::run() {
+    while(mWindow.isOpen()){
+        sf::Event event;
+        while(mWindow.pollEvent(event)){
+            if(event.type == sf::Event::Closed) mWindow.close();
+
+            mCalculator.handleEvents(event);
+        }
+        update();
+        render();
+    }
+}
+
+void Application::update() {
+    mCalculator.update();
+}
+
+void Application::render() {
+    mWindow.clear(sf::Color::Blue);
+    mWindow.draw(mCalculator);
+    mWindow.display();
+}
+
